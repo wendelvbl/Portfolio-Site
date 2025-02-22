7db41ac1ad5a51ec60791bc1ab6ed8415a4f849b
wendelvbl <wendel.vanbrusselenlanduyt@student.ap.be>
2025-02-22 09:02:37 +0100

    Week 3 post

--- portfoliowebsite/src/context/context.tsx
+++ portfoliowebsite/src/context/context.tsx
@@ -80,6 +80,13 @@ const dummyPosts: BlogPost[] = [
       "The second week flew by in an instant! I learned a lot of new things, mainly related to authentication. Some of the key tasks I worked on included generating JWT tokens, saving them to a database, and using a CRON job for the first time to send emails. I also authenticated those tokens when they were used. One major takeaway is that I need to focus more on testing. I wrote too much code before verifying that everything worked as expected, which led to me spending three-quarters of Wednesday debugging. All in all, a successful week!",
     tags: ["back-end", "node", "Februari"],
   },
+  {
+    id: 4,
+    date: "2025-02-21",
+    title: "Reflection after week 3",
+    message:"The third week went by just as fast! This time, I focused on optimizing my CSS and improving the responsiveness of my project. I also designed an email layout for a mail, which was a fun challenge. On the backend, I created a POST API controller, making sure everything worked smoothly. Another big highlight was working with Redux, a state management tool that helped streamline data flow in my project. One key takeaway is that proper state management can make a huge difference in maintaining clean and efficient code. Overall, a productive and insightful week!",
+    tags: ["back-end", "node", "Februari","front-end"],
+  },
 ];
 
 export default dummyPosts;
